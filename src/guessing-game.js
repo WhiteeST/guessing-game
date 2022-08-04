9e93f1fe7280540300fe43625cdc7954b46d8734
WhiteeST <whiteeluck123@gmail.com>
2022-08-04 12:14:14 +0400

    feat: game over

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() { }
+    ;
     setRange(min, max) {
-
+        this.rangeMin = min;
+        this.rangeMax = max;
+        this.lastGuess = Math.round((this.rangeMin + this.rangeMax) / 2);
     }
 
     guess() {
-
+        this.lastGuess = Math.round((this.rangeMin + this.rangeMax) / 2);
+        return this.lastGuess;
     }
 
     lower() {
-
+        this.rangeMax = this.lastGuess
     }
 
     greater() {
-
+        this.rangeMin = this.lastGuess
     }
 }
 
